fb5a1cb7397b7a78aa2becc4513ab25800b6f78f
polynomeer <polynomeer@gmail.com>
2020-12-04 01:06:24 +0900

    feat: Change SignUp fields and others

--- src/swing/SignUp.java
+++ src/swing/SignUp.java
@@ -26,150 +26,115 @@ public class SignUp extends JDialog {
         setBackground(LedgerMain.BG_COLOR); // this changes color including title bar
         setLayout(null); // this make you can set layout manually
 
-        lblId = new JLabel("id");
-        lblPwd = new JLabel("password");
-        lblName = new JLabel("name");
-        lblHp = new JLabel("phone");
-        lblGen = new JLabel("gender");
-        lblHobby = new JLabel("hobby");
-        lblJob = new JLabel("job");
-        lblAddr = new JLabel("address");
-        lblAge = new JLabel("age");
+        lbId = new JLabel("id");
+        lbPwd = new JLabel("password");
+        lbName = new JLabel("name");
+        lbHp = new JLabel("phone");
+        lbGen = new JLabel("gender");
+        lbHobby = new JLabel("hobby");
+        lbJob = new JLabel("job");
+        lbAddr = new JLabel("address");
+        lbAge = new JLabel("age");
 
-//        lblAge.setFont(new Font("Tahoma", Font.PLAIN,14));
-        lblId.setFont(LedgerMain.BASIC_FONT); // NOI18N
-        lblPwd.setFont(LedgerMain.BASIC_FONT);
-        lblName.setFont(LedgerMain.BASIC_FONT);
-        lblHp.setFont(LedgerMain.BASIC_FONT);
-        lblGen.setFont(LedgerMain.BASIC_FONT);
-        lblJob.setFont(LedgerMain.BASIC_FONT);
-        lblAddr.setFont(LedgerMain.BASIC_FONT);
-        lblAge.setFont(LedgerMain.BASIC_FONT);
+//        lbAge.setFont(new Font("Tahoma", Font.PLAIN,14));
+        lbId.setFont(LedgerMain.BASIC_FONT); // NOI18N
+        lbPwd.setFont(LedgerMain.BASIC_FONT);
+        lbName.setFont(LedgerMain.BASIC_FONT);
+        lbHp.setFont(LedgerMain.BASIC_FONT);
+        lbGen.setFont(LedgerMain.BASIC_FONT);
+        lbJob.setFont(LedgerMain.BASIC_FONT);
+        lbAddr.setFont(LedgerMain.BASIC_FONT);
+        lbAge.setFont(LedgerMain.BASIC_FONT);
 
-        lblId.setForeground(Color.WHITE);
-        lblPwd.setForeground(Color.WHITE);
-        lblName.setForeground(Color.WHITE);
-        lblHp.setForeground(Color.WHITE);
-        lblGen.setForeground(Color.WHITE);
-        lblHobby.setForeground(Color.WHITE);
-        lblJob.setForeground(Color.WHITE);
-        lblAddr.setForeground(Color.WHITE);
-        lblAge.setForeground(Color.WHITE);
+        lbId.setForeground(Color.WHITE);
+        lbPwd.setForeground(Color.WHITE);
+        lbName.setForeground(Color.WHITE);
+        lbHp.setForeground(Color.WHITE);
+        lbGen.setForeground(Color.WHITE);
+        lbHobby.setForeground(Color.WHITE);
+        lbJob.setForeground(Color.WHITE);
+        lbAddr.setForeground(Color.WHITE);
+        lbAge.setForeground(Color.WHITE);
 
-        lblId.setBounds(20, 50, 100, 20);
-        lblPwd.setBounds(20, 80, 100, 20);
-        lblName.setBounds(20, 110, 100, 20);
-        lblHp.setBounds(20, 140, 100, 20);
-        lblGen.setBounds(20, 170, 100, 20);
-        lblHobby.setBounds(20, 200, 100, 20);
-        lblJob.setBounds(20, 230, 100, 20);
-        lblAddr.setBounds(20, 260, 100, 20);
-        lblAge.setBounds(20, 290, 100, 20);
+        lbId.setBounds(20, 50, 100, 20);
+        lbPwd.setBounds(20, 80, 100, 20);
+        lbName.setBounds(20, 110, 100, 20);
+        lbHp.setBounds(20, 140, 100, 20);
+        lbGen.setBounds(20, 170, 100, 20);
+        lbHobby.setBounds(20, 200, 100, 20);
+        lbJob.setBounds(20, 230, 100, 20);
+        lbAddr.setBounds(20, 260, 100, 20);
+        lbAge.setBounds(20, 290, 100, 20);
 
-        add(lblId);
-        add(lblPwd);
-        add(lblName);
-        add(lblHp);
-        add(lblGen);
-        add(lblHobby);
-        add(lblJob);
-        add(lblAddr);
-        add(lblAge);
+        add(lbId);
+        add(lbPwd);
+        add(lbName);
+        add(lbHp);
+        add(lbGen);
+        add(lbHobby);
+        add(lbJob);
+        add(lbAddr);
+        add(lbAge);
 
         txtId = new JTextField(20);
         txtPwd = new JTextField(20);
         txtName = new JTextField(20);
-        Label lblhipen1 = new Label("-");
-        Label lblhipen2 = new Label("-");
+        Label lbHipen1 = new Label("-");
+        lbHipen1.setForeground(Color.WHITE);
+        Label lbHipen2 = new Label("-");
+        lbHipen2.setForeground(Color.WHITE);
         txtHp1 = new JTextField();
         txtHp2 = new JTextField();
         txtHp3 = new JTextField();
-
-        panGen = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        panHobby = new JPanel(new FlowLayout(FlowLayout.LEFT));
-
-        CheckboxGroup group = new CheckboxGroup();
-        cbMale = new Checkbox("male", group, true);
-        cbMale.setFont(LedgerMain.BASIC_FONT);
-        cbMale.setForeground(Color.WHITE);
-        cbFeMale = new Checkbox("female", group, false);
-        cbFeMale.setFont(LedgerMain.BASIC_FONT);
-        cbFeMale.setForeground(Color.WHITE);
-
-        panGen.add(cbMale);
-        panGen.add(cbFeMale);
-        panGen.setBounds(120, 162, 150, 30);
-        panGen.setBackground(LedgerMain.BG_COLOR);
-
-        cbHobby1 = new Checkbox("cook");
-        cbHobby1.setFont(LedgerMain.BASIC_FONT);
-        cbHobby1.setForeground(Color.WHITE);
-        cbHobby2 = new Checkbox("games");
-        cbHobby2.setFont(LedgerMain.BASIC_FONT);
-        cbHobby2.setForeground(Color.WHITE);
-        cbHobby3 = new Checkbox("books");
-        cbHobby3.setFont(LedgerMain.BASIC_FONT);
-        cbHobby3.setForeground(Color.WHITE);
-
-        panHobby.add(cbHobby1);
-        panHobby.add(cbHobby2);
-        panHobby.add(cbHobby3);
-
-        panHobby.setBounds(120, 192, 250, 30);
-        panHobby.setBackground(LedgerMain.BG_COLOR);
-
-        chJob = new Choice();
-        chJob.add("Programmer");
-        chJob.add("Doctor");
-        chJob.add("Professor");
-        chJob.add("Designer");
-
         txtAddr = new JTextField();
         txtAge = new JTextField();
+        txtGender = new JTextField();
+        txtJob = new JTextField();
+        txtHobby = new JTextField();
 
-        btnCancel = new JButton("cancel");
-        btnSubmit = new JButton("sign up");
-        //btnCancel.setSize(150, 40);
-        //btnSubmit.setSize(150, 40);
+        btnCancel = new JButton("Cancel");
+        btnSubmit = new JButton("Sign up");
 
         txtId.setBounds(120, 50, 150, 20);
         txtPwd.setBounds(120, 80, 150, 20);
         txtName.setBounds(120, 110, 150, 20);
         txtHp1.setBounds(120, 140, 60, 20);
-        lblhipen1.setBounds(190, 140, 10, 20);
+        lbHipen1.setBounds(190, 140, 10, 20);
         txtHp2.setBounds(210, 140, 60, 20);
-        lblhipen2.setBounds(275, 140, 10, 20);
+        lbHipen2.setBounds(275, 140, 10, 20);
         txtHp3.setBounds(290, 140, 60, 20);
-
-        chJob.setBounds(120, 230, 150, 20);
+        txtGender.setBounds(120,170,150,20);
+        txtJob.setBounds(120,200,150,20);
+        txtHobby.setBounds(120,230,150,20);
         txtAddr.setBounds(120, 260, 150, 20);
         txtAge.setBounds(120, 290, 150, 20);
 
-        paButton = new JPanel();
-        paButton.add(btnSubmit);
-        paButton.add(btnCancel);
-        paButton.setBounds(0, 320, 370, 370);
-        paButton.setBackground(LedgerMain.BG_COLOR);
+        paneButton = new JPanel();
+        paneButton.add(btnSubmit);
+        paneButton.add(btnCancel);
+        paneButton.setBounds(0, 320, 370, 370);
+        paneButton.setBackground(LedgerMain.BG_COLOR);
 
         add(txtId);
         add(txtPwd);
         add(txtName);
         add(txtHp1);
-        add(lblhipen1);
+        add(lbHipen1);
         add(txtHp2);
-        add(lblhipen2);
+        add(lbHipen2);
         add(txtHp3);
-        add(panGen);
-        add(panHobby);
-        add(chJob);
+        add(txtGender);
+        add(txtJob);
+        add(txtHobby);
         add(txtAddr);
         add(txtAge);
-        add(paButton);
+        add(paneButton);
 
         btnSubmit.addActionListener(e -> {
 //                label.setText(field.getText());
             boolean isInserted = insertUser();
             if (isInserted) {
+                JOptionPane.showMessageDialog(null, "Thank you for signing up! Welcome to Ledger!");
                 dispose();
             }
 
@@ -180,21 +145,16 @@ public class SignUp extends JDialog {
     private boolean insertUser() {
 
         try {
-            User user;
             String id = txtId.getText();
             String password = txtPwd.getText();
             String name = txtName.getText();
             String phone = txtHp1.getText() + "-" + txtHp2.getText() + "-" + txtHp3.getText();
-//            String gender = lblGen.getText();
-//            String job = lblJob.getText();
-//            String hobby = lblHobby.getText();
-            String gender = "F";
-            String job = "Programmer";
-            String hobby = "Soccer";
+            String gender = txtGender.getText();
+            String job = txtJob.getText();
+            String hobby = txtHobby.getText();
+
             String address = txtAddr.getText();
             int age = Integer.parseInt(txtAge.getText());
-
-            user = new User(id, password, name, phone, gender.charAt(0), job, hobby, address, age);
 
             try {
                 PreparedStatement pstmt = connection.prepareStatement("INSERT INTO user (id, password, name, phone, gender, job, hobby, address, age) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
@@ -208,11 +168,12 @@ public class SignUp extends JDialog {
                 pstmt.setString(8, address);
                 pstmt.setInt(9, age);
                 pstmt.executeUpdate();
-                System.out.println("Insert success!!!");
+                System.out.println("Insert Success!");
+                return true;
+
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(null, "Error in JTable ..... " + e);
             }
-            return true;
 
         } catch (NumberFormatException numberFormatException) {
             JOptionPane.showMessageDialog(null, "Error in signing up: " + numberFormatException);
@@ -221,14 +182,9 @@ public class SignUp extends JDialog {
         return false;
     }
 
-    JPanel paButton, panGen, panHobby;
-    JLabel lblId, lblPwd, lblName, lblHp, lblGen, lblHobby, lblJob, lblAddr, lblAge;
-    JTextField txtId, txtPwd, txtName;
-    JTextField txtHp1, txtHp2, txtHp3;
-    Checkbox cbMale, cbFeMale;
-    Checkbox cbHobby1, cbHobby2, cbHobby3;
-    Choice chJob;
-    JTextField txtAddr, txtAge;
+    JPanel paneButton;
+    JLabel lbId, lbPwd, lbName, lbHp, lbGen, lbHobby, lbJob, lbAddr, lbAge;
+    JTextField txtId, txtPwd, txtName, txtHp1, txtHp2, txtHp3, txtGender, txtJob, txtHobby, txtAddr, txtAge;
     JButton btnSubmit, btnCancel;
 
 }
